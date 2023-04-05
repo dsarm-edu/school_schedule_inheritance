b12fd7a2338fe944db9c390e3d62d875999934ae
Danica <danicas.edu@gmail.com>
2023-04-05 11:56:29 -0700

    created MiddleSchoolStudent object and corresponding tests

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,13 @@
 from .student import Student
 
-# add MiddleSchoolStudent here
+
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+
+    def summary(self):
+        first_line = super().summary()
+        second_line = f"{self.name} gets transportation: {self.gets_transportation}"
+        return f"{first_line} {second_line}" 
+        # this combines summary from student.py + MiddleSchool 
